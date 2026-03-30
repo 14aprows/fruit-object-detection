9a18a3659d3bad51a29f388fdde10c39f518894e
Veerman14 <Hasibuanfirman1404@gmail.com>
2026-03-30 23:57:07 +0700

    refactor: improve data augmentation pipeline

--- dataset/preprocessing.py
+++ dataset/preprocessing.py
@@ -8,9 +8,15 @@ def get_train_transform():
             A.HorizontalFlip(p=0.5),
             A.RandomBrightnessContrast(p=0.2),
             A.Blur(blur_limit=3, p=0.1),
+
+            A.Normalize(
+                mean=(0.485, 0.456, 0.406),
+                std=(0.229, 0.224, 0.225)
+            ),
+
             ToTensorV2()
-        ], 
-        bbox_params = A.BboxParams(
+        ],
+        bbox_params=A.BboxParams(
             format="pascal_voc",
             label_fields=["labels"]
         )
@@ -20,9 +26,15 @@ def get_valid_transform():
     return A.Compose(
         [
             A.Resize(512, 512),
+
+            A.Normalize(
+                mean=(0.485, 0.456, 0.406),
+                std=(0.229, 0.224, 0.225)
+            ),
+
             ToTensorV2()
-        ], 
-        bbox_params = A.BboxParams(
+        ],
+        bbox_params=A.BboxParams(
             format="pascal_voc",
             label_fields=["labels"]
         )
